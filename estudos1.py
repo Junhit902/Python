fb06477bb5fc4ae7af299cb31edc40a90a669a22
junhit-coder <thiagojunhonma@gmail.com>
2024-06-09 19:49:30 -0300

    primeira semana

---
+++ estudos1.py
@@ -0,0 +1,15 @@
+"""
+DocString
+E escrever o que eu
+quiser
+
+
+asdfasdfd
+"""
+
+''' Usar para escrever suas notas '''
+
+# Permite escrever uma linha de comentário
+print(123)  # Na frente
+# Abaixo
+print(456)
---
+++ estudos3.py
@@ -0,0 +1,24 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Thiago Jun Honma')
+print(1, 'Luiz "Thiago Jun Honma"')
+
+# Aspas duplas
+print("Thiago Jun Honma")
+print(2, "Thiago Jun Honma'")
+
+# Escape
+print("Thiago \"Jun\"")
+
+# r
+print(r"Thiago \"Jun\"")
+
+#Alternar as aspas simples e duplas, caso queira colocar dentro da função print
