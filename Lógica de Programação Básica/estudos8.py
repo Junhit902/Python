d16239851f40c05f3c10d097879cb5631b660737
junhit-coder <thiagojunhonma@gmail.com>
2024-06-09 22:29:00 -0300

    1 dia

---
+++ Lógica de Programação Básica/estudos8.py
@@ -0,0 +1,15 @@
+nome = 'Thiago' # String
+sobrenome = 'Honma' # String
+idade = 21 # int
+ano_nascimento = 2024 - idade # int
+maior_de_idade = idade >= 18 #Boolean
+altura_metros = 1.85 # float
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros: ' +str(altura_metros)+ 'm') # Podemos concatenar(juntar) palavras e argumentos com o
+# operador aritmetico (+), no caso do exemplo acima, a altura em metros foi convertida de float para String
+# pois o python não consegue concatenar argumentos de tipos diferentes
