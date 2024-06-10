94f6af07729aa790e206f8bdfd5ced1856421a98
junhit-coder <thiagojunhonma@gmail.com>
2024-06-09 22:13:39 -0300

    2 dia

---
+++ estudos6.py
@@ -0,0 +1,11 @@
+# conversão de tipos, coerção
+# type convertion, typecasting, coercion
+# é o ato de converter um tipo em outro tipo
+#  tipos imutáveis e primitivos: str, int, float, bool
+# Exemplo: '0' é uma string, pois está dentro de aspas simples
+# Se colocarmos assim int('0'), a string é convertido para int
+
+print(int('1'), type(int('1')))
+print(type(float('1') + 1))
+print(bool(' '))
+print(str(11) + 'b')
---
+++ estudos7.py
@@ -0,0 +1,18 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP8: inicie variáveis com letras minúsculas, pode usar
+# números e underline _.
+# O sinal de = é o operador de atribuição. Ele é usado para
+# atribuir um valor a um nome (variável).
+# Uso: nome_variavel = expressão
+
+# nome_completo = 'Luiz Otávio Miranda'
+# soma_dois_mais_dois = 2 + 2
+# int_um = bool('1')
+# print(int_um, type(int_um))
+# print(nome_completo, soma_dois_mais_dois)
+
+nome = 'Thiago'
+idade = 21
+maior_de_idade = idade >= 18 # Vai mostrar se é maior ou não que 18, se naõ for é false caao contrário true
+print('Nome:', nome, 'Idade:', idade)
+print('É maior?', maior_de_idade)
