24f2eb28308f29a33aedc6d500837fdd35222e67
junhit-coder <thiagojunhonma@gmail.com>
2024-06-25 20:29:44 -0300

    25/06/2024

--- Lógica de Programação Básica/estudos12.py
+++ Lógica de Programação Básica/estudos12.py
@@ -3,7 +3,7 @@
 nome = 'Thiago Jun'
 altura = 1.85
 peso = 68
-imc = (peso/(altura ** 2))
+imc = (peso/(altura ** 2)) # peso / (altura * altura)
 
 print('Seu nome é ' + nome + ', você tem ' + str(altura) + 'm de altura e ' + str(peso) + ' quilos e seu IMC é ' + str(imc))
 
