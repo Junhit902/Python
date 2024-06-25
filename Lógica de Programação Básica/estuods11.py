246c71bad4b1015bc90bad1a9957149d3d3fe19c
junhit-coder <thiagojunhonma@gmail.com>
2024-06-25 18:54:19 -0300

    25/06/2024

---
+++ Lógica de Programação Básica/estuods11.py
@@ -0,0 +1,9 @@
+#Precedência em operadores aritméticos (prioridade)
+
+# 1. (n + n)
+# 2. **
+# 3. *, /, //, %
+# 4. +, -
+
+conta_1 = (((1+1) + 2) ** 2) / 4     # Resultado = 4
+print(int(conta_1))
