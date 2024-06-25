c0076d853c46fa17733c5ad80178ffd2230f525d
junhit-coder <thiagojunhonma@gmail.com>
2024-06-25 20:27:26 -0300

    25/06/2024

---
+++ Lógica de Programação Básica/estudos12.py
@@ -0,0 +1,10 @@
+# Calculando o IMC ([Indice de Massa Corporal)
+
+nome = 'Thiago Jun'
+altura = 1.85
+peso = 68
+imc = (peso/(altura ** 2))
+
+print('Seu nome é ' + nome + ', você tem ' + str(altura) + 'm de altura e ' + str(peso) + ' quilos e seu IMC é ' + str(imc))
+
+print('Seu nome é', nome,'você tem', altura, 'de altura e', peso, 'quilos e seu IMC é', imc)
