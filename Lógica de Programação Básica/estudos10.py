0ad52c77052c43c04ffa4776e9721cd69b3e2c3d
junhit-coder <thiagojunhonma@gmail.com>
2024-06-25 18:36:45 -0300

    25/06/2024

---
+++ Lógica de Programação Básica/estudos10.py
@@ -0,0 +1,8 @@
+concatenacao = 'Thiago' + 'Jun' + 'Honma' #A concatenção(juntar) só pode ser feita em String apenas
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10
+print(a_dez_vezes)
+
+thiago_duas_vezes = 'Thiago' * 2
+print(thiago_duas_vezes)
