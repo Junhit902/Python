f68dab51aa578af9b0d85fb27557d22d251e916d
junhit-coder <thiagojunhonma@gmail.com>
2025-02-06 01:54:42 -0300

    atualizado

--- 100 Days Python Bootcamp/Day4/day4.py
+++ 100 Days Python Bootcamp/Day4/day4.py
@@ -133,25 +133,28 @@ tesoura = """
         ...-===++=+++%@@+:...                                .                                  .
             .....:=+++*@#-..                   .                       .         .                .                        .                      .             
 """
-if escolha == 0:
-    print(f"Você escolheu pedra: \n{pedra}")
-elif escolha == 1:
-    print(f"Você escolheu papel: \n{papel}")
-elif escolha == 2:
-    print(f"Você escolheu tesoura: \n{tesoura}")
+if 0 <= escolha < 3:
+
+    if escolha == 0:
+        print(f"Você escolheu pedra: \n{pedra}")
+    elif escolha == 1:
+        print(f"Você escolheu papel: \n{papel}")
+    elif escolha == 2:
+        print(f"Você escolheu tesoura: \n{tesoura}")
+
+
+
+    opcoes = [pedra, papel, tesoura]
+    computador = random.randint(0, 2)
+    print("O computador escolheu:")
+    print(opcoes[computador])
+
+
+    if (escolha == 0 and computador == 1) or (escolha == 1 and computador == 2) or (escolha == 2 and computador == 0):
+        print("Você perdeu, mais sorte na próxima vez!")
+    elif (escolha == 0 and computador == 2) or (escolha == 1 and computador == 0) or (escolha == 2 and computador == 1):
+        print("Você ganhou, parabéns!")
+    elif escolha == computador:
+        print("Empate")
 else:
     print("Escolha a opção correta!")
-
-
-opcoes = [pedra, papel, tesoura]
-computador = random.randint(0, 2)
-print("O computador escolheu:")
-print(opcoes[computador])
-
-
-if (escolha == 0 and computador == 1) or (escolha == 1 and computador == 2) or (escolha == 2 and computador == 0):
-    print("Você perdeu, mais sorte na próxima vez!")
-elif (escolha == 0 and computador == 2) or (escolha == 1 and computador == 0) or (escolha == 2 and computador == 1):
-    print("Você ganhou, parabéns!")
-elif escolha == computador:
-    print("Empate")
