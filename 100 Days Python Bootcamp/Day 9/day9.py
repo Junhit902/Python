23c6fc14b0e4790f4b9301eb29dd569bc545db2e
junhit-coder <thiagojunhonma@gmail.com>
2025-03-07 18:33:07 -0300

    07/03/2025

--- 100 Days Python Bootcamp/Day 9/day9.py
+++ 100 Days Python Bootcamp/Day 9/day9.py
@@ -1,14 +1,26 @@
-letras = ["A", "B", ["C", "D"]]
+import os
 
-print(letras[2][0])
+leilao: dict[str, float]={}
+yes_no = True
 
+while yes_no:
+    nome = input("Qual é o seu nome?: ")
+    lance = float(input("Quanto é o seu lance: R$"))
 
-cidades: dict[str, list[str]] = {
-"Japao": {"provincias": ["Tokyo", "Osaka", "Gunma", "Miyagi"], 
-            "numero_vezes_visitado": 4
-        },
-"Brasi": ["Sao Paulo", "Rio de Janeiro", "Bahia", "Paraná"]
-}
+    leilao[nome] = lance
 
+    continuar = input("Você quer continuar? Digite 'S'(Sim) ou 'N'(Não): ").upper()
 
-print(cidades["Japao"]["provincias"][2])
+    if continuar == "S":
+        yes_no = True
+        os.system("cls")
+    elif continuar == "N":
+        yes_no = False
+        os.system("cls")
+        lance_maior = 0
+        nome_maior = ""
+        for nomes, lances in leilao.items():
+            if lances > lance_maior:
+                nome_maior = nomes
+                lance_maior = lances
+        print(f"O maior lance foi {nome_maior} com o valor de R${lance_maior:.2f}")
