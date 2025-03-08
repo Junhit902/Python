d5557d4ce38b4da2b0ca0511b69653ad76c404c9
junhit-coder <thiagojunhonma@gmail.com>
2025-03-08 19:26:38 -0300

    08/03/2025

--- 100 Days Python Bootcamp/Day 10/day10.py
+++ 100 Days Python Bootcamp/Day 10/day10.py
@@ -1,17 +1,15 @@
 def is_leap_year(year):
     if year % 4 == 0:
-        print("Leap")
         if year % 100 == 0:
-            print("Not Leap")
-            if year % 400 != 0:
-                print("Not Leap")
-                return False
-            else:
-                print("Leap")
+            if year % 400 == 0:
                 return True
+            else:
+                return False
+        else:
+            return True
     else:
-        print("Not Leap")
         return False
+    
+output = is_leap_year(year=2024)
 
-output = is_leap_year(year=int(input("Digite um ano qualquer: ")))
 print(output)
