9e0871537c49e3877332447e8bc28a56ac64a83d
junhit-coder <thiagojunhonma@gmail.com>
2025-03-08 22:36:20 -0300

    08/03/2025

--- 100 Days Python Bootcamp/Day 10/day10.py
+++ 100 Days Python Bootcamp/Day 10/day10.py
@@ -1,15 +1,56 @@
-def is_leap_year(year):
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 == 0:
-                return True
-            else:
-                return False
-        else:
-            return True
-    else:
-        return False
-    
-output = is_leap_year(year=2024)
+import os
 
-print(output)
+def soma(n1, n2):
+    soma = n1 + n2
+    return soma
+
+def subtracao(n1, n2):
+    subtracao = n1 - n2
+    return subtracao
+
+def multiplicacao(n1, n2):
+    multiplicacao = n1 * n2
+    return multiplicacao
+
+def divisao(n1,n2):
+    divisao = n1 / n2
+    return divisao
+
+calculadora: dict[str,float]={
+    "+": soma,
+    "-": subtracao,
+    "*": multiplicacao,
+    "/": divisao
+}
+
+resetar = True
+
+numero1 = float(input("Digite um número: "))
+
+while resetar:
+    numero2 = float(input("Digite um segundo número: "))
+
+    print("\nEscolha uma dessas operações que deseja realizar:")
+    for simbolo in calculadora:
+        print(simbolo)
+
+    operacao = input("\nDigite qual operacão deseja realizar: ")
+    
+    if operacao in calculadora:
+        resultado = calculadora[operacao](numero1, numero2)
+        print(f"Resultado: {numero1} {operacao} {numero2} = {resultado}")
+    else:
+        os.system("cls")
+        print("Digite uma operação correta!\n")
+        break
+
+    continuar = input(f"\nVocê quer continuar a fazer mais uma operação com o resultado({resultado})? Digite (s)Sim ou (n)Não: ").lower()
+
+    if continuar == "s":
+        os.system("cls")
+        numero1 = resultado
+        print(f"Resultado anterior = {resultado}")
+    elif continuar == "n":
+        os.system("cls")
+        print("Desligando...")
+        resetar = False
