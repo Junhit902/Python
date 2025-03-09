a2a9c585ac454ea94a9ea37fd7ba4a386ef40c67
junhit-coder <thiagojunhonma@gmail.com>
2025-03-08 23:55:33 -0300

    08/03/2025

--- 100 Days Python Bootcamp/Day 10/day10.py
+++ 100 Days Python Bootcamp/Day 10/day10.py
@@ -23,34 +23,36 @@ calculadora: dict[str,float]={
     "/": divisao
 }
 
-resetar = True
+def calc():
+    resetar = True
+    numero1 = float(input("Digite um número: "))
 
-numero1 = float(input("Digite um número: "))
+    while resetar:
+        numero2 = float(input("Digite um segundo número: "))
 
-while resetar:
-    numero2 = float(input("Digite um segundo número: "))
+        print("\nEscolha uma dessas operações que deseja realizar:")
+        for simbolo in calculadora:
+            print(simbolo)
 
-    print("\nEscolha uma dessas operações que deseja realizar:")
-    for simbolo in calculadora:
-        print(simbolo)
+        operacao = input("\nDigite qual operacão deseja realizar: ")
+        
+        if operacao in calculadora:
+            resultado = calculadora[operacao](numero1, numero2)
+            print(f"Resultado: {numero1} {operacao} {numero2} = {resultado}")
+        else:
+            os.system("cls")
+            print("Digite uma operação correta!\n")
+            break
 
-    operacao = input("\nDigite qual operacão deseja realizar: ")
-    
-    if operacao in calculadora:
-        resultado = calculadora[operacao](numero1, numero2)
-        print(f"Resultado: {numero1} {operacao} {numero2} = {resultado}")
-    else:
-        os.system("cls")
-        print("Digite uma operação correta!\n")
-        break
+        continuar = input(f"\nVocê quer continuar a fazer mais uma operação com o resultado({resultado})? Digite (s)Sim ou (n)Não para resetar o resultado: ").lower()
 
-    continuar = input(f"\nVocê quer continuar a fazer mais uma operação com o resultado({resultado})? Digite (s)Sim ou (n)Não: ").lower()
+        if continuar == "s":
+            os.system("cls")
+            numero1 = resultado
+        elif continuar == "n":
+            os.system("cls")
+            print("Resetando...")
+            resetar = False
+            calc()
 
-    if continuar == "s":
-        os.system("cls")
-        numero1 = resultado
-        print(f"Resultado anterior = {resultado}")
-    elif continuar == "n":
-        os.system("cls")
-        print("Desligando...")
-        resetar = False
+calc()
