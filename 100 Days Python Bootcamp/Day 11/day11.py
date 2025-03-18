50c9bbdad6aff79992329789c85ae52275788e54
junhit-coder <thiagojunhonma@gmail.com>
2025-03-18 14:29:26 -0300

    alterações feitas

--- 100 Days Python Bootcamp/Day 11/day11.py
+++ 100 Days Python Bootcamp/Day 11/day11.py
@@ -1,6 +1,48 @@
 import random
 
 cartas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 11]
+cartas_comp = []
+cartas_jogador = []
 
-carta_comp = []
-carta_jogador = []
+contador = 2
+
+def distribuir_cartas():
+    for carta in range(contador):
+        carta_c = random.choice(cartas)
+        carta_j = random.choice(cartas)
+        cartas_comp.append(carta_c)
+        cartas_jogador.append(carta_j)
+
+def pegar_mais_carta():
+    carta_j = random.choice(cartas)
+    cartas_jogador.append(carta_j)
+
+distribuir_cartas()
+
+print(f"{cartas_comp} = Computador")
+print(f"{cartas_jogador} = Jogador")
+
+ponto_comp = sum(cartas_comp)
+ponto_jogador = sum(cartas_jogador)
+
+print(f"Pontos do Computador = {ponto_comp}")
+print(f"Pontos do jogador = {ponto_jogador}")
+
+
+if ponto_jogador < 21:
+    carta_mesa = input("[1]Pegar mais uma carta \n[2]Jogar na mesa \nEscolha umas dessas opções: ")
+    if carta_mesa == "1":
+        pegar_mais_carta()
+        ponto_jogador = sum(cartas_jogador)
+        print(f"Pontos do jogador = {ponto_jogador}")
+    elif carta_mesa == "2":
+        if ponto_jogador > ponto_comp:
+            print("Você ganhou! Parabéns.")
+        elif ponto_jogador < ponto_comp:
+            print("Você perdeu!")
+        elif ponto_jogador == ponto_comp:
+            print("Empate!")
+elif ponto_jogador == 21:
+    print("Você ganhou!")
+else:
+    print("Você perdeu!")
