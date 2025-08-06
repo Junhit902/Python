7d1ec485986144de1c90f14239cc06517b29ea5b
Juntjh <thiagojunhonma@gmail.com>
2025-08-05 22:11:03 -0300

    day14 - pequenas alterações

--- 100 Days Python Bootcamp/Day 14/day14.py
+++ 100 Days Python Bootcamp/Day 14/day14.py
@@ -6,6 +6,7 @@ import os
 perdeu = False
 nova_pontuação = 0
 pontuacao_total = 0
+pais_b = random.choice(dados_paises)
 
 def comparacao(pais1, pais2):
     pop1 = dados_paises[pais1]['populacao']
@@ -37,8 +38,8 @@ def resultado(opcao, pais_correta, pontos, pais1, pais2, pontuacao_final):
 while perdeu is False:
     print(ascart.titulo)
     print("Qual é o país que possui a maior população?")
-    pais_a = random.choice(range(0, len(dados_paises)))
-    pais_b = random.choice(range(0, len(dados_paises)))
+    pais_a = pais_b
+    pais_b = random.choice(dados_paises)
     print(f"\nPaís A: {dados_paises[pais_a]["pais"]} \nÁrea(km²): {dados_paises[pais_a]["area_km2"]} \nDescrição: {dados_paises[pais_a]["descricao"]}")
     print(ascart.vs)
     print(f"\nPaís B: {dados_paises[pais_b]["pais"]} \nÁrea(km²): {dados_paises[pais_b]["area_km2"]} \nDescrição: {dados_paises[pais_b]["descricao"]}")
