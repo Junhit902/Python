f90680130b7e10835d0039b3c619ee00102d0c22
Juntjh <thiagojunhonma@gmail.com>
2025-08-12 00:48:40 -0300

    tartaruga

--- 100 Days Python Bootcamp/Day 16/day16.py
+++ 100 Days Python Bootcamp/Day 16/day16.py
@@ -1,1 +1,10 @@
-#POO no Python
+from turtle import Turtle, Screen
+
+tartaruga = Turtle()
+print(tartaruga)
+tartaruga.shape("turtle")
+tartaruga.color("red", "green")
+
+minha_tela = Screen()
+print(minha_tela.canvheight)
+minha_tela.exitonclick()
