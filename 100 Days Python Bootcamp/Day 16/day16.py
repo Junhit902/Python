183b65ae29434faaaa7d65840d7a81df471ed689
Juntjh <thiagojunhonma@gmail.com>
2025-08-12 16:38:32 -0300

    day 16 e day 17 em progresso

--- 100 Days Python Bootcamp/Day 16/day16.py
+++ 100 Days Python Bootcamp/Day 16/day16.py
@@ -4,5 +4,6 @@ mesa = prettytable.PrettyTable()
 
 mesa.add_column("Pokemon",["Pikachu","Squirtle","Charmander"])
 mesa.add_column("Tipo", ["Elétrico", "Água", "Fogo"])
+mesa.align = "l"
 
 print(mesa)
