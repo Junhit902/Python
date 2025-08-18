37df66a9972f3b9c9aac7fca2f486bbe759bbb41
Juntjh <thiagojunhonma@gmail.com>
2025-08-18 15:31:05 -0300

    tartaruga colormode - concluído

--- 100 Days Python Bootcamp/Day 18/day18.py
+++ 100 Days Python Bootcamp/Day 18/day18.py
@@ -1,18 +1,30 @@
-from turtle import Turtle, Screen
-from cores import cores_variados
-import random as r
+import turtle as t
+from turtle import Screen
+import random
 
 angulos = [0, 90, 180, 270] # Modo standard 0 = leste, 90 = norte, 180 = oeste e 270 = sul
 
-tartaruga = Turtle()
+tartaruga = t.Turtle()
+t.colormode(255)
+
+def cores_aleatorias():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+
+    core_aleat = (r, g, b)
+
+    return core_aleat
+
 tela = Screen()
 tartaruga.pensize(10)
-tartaruga.speed(0) # Poderia colocar como string 0 = "fastest", 10 = "fast", 6 = "normal", 
+tartaruga.speed(10) # Poderia colocar como string 0 = "fastest", 10 = "fast", 6 = "normal", 
 #"slow" = 3, "slowest" = 1    Exemplo: tartaruga.speed("fastest")
 
+
 for _ in range(300):
-    tartaruga.pencolor(r.choice(cores_variados))
+    tartaruga.pencolor(cores_aleatorias())
     tartaruga.forward(30)
-    tartaruga.setheading(r.choice(angulos))
+    tartaruga.setheading(random.choice(angulos))
     
 tela.exitonclick()
