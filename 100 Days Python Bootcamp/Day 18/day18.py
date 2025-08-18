9b27bbfedbd9f6a11169b352f6bb6af7d4bd2539
Juntjh <thiagojunhonma@gmail.com>
2025-08-18 00:38:43 -0300

    tartaruga linha tracejada

--- 100 Days Python Bootcamp/Day 18/day18.py
+++ 100 Days Python Bootcamp/Day 18/day18.py
@@ -6,10 +6,11 @@ tela = Screen()
 tartaruga.shape("turtle")
 tartaruga.color("green")
 
-for _ in range(4):
-    tartaruga.forward(100)
-    tartaruga.left(90)
-
+for _ in range(50):
+    tartaruga.forward(10)
+    tartaruga.penup()
+    tartaruga.forward(10)
+    tartaruga.pendown()
 
 
 tela.exitonclick()
