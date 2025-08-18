8ad05d4365e15f1c76300ff2633df000aca95d97
Juntjh <thiagojunhonma@gmail.com>
2025-08-18 15:04:24 -0300

    tartaruga movimentos aleatórios - concluído

--- 100 Days Python Bootcamp/Day 18/day18.py
+++ 100 Days Python Bootcamp/Day 18/day18.py
@@ -1,20 +1,18 @@
 from turtle import Turtle, Screen
-import random
-import cores
+from cores import cores_variados
+import random as r
+
+angulos = [0, 90, 180, 270] # Modo standard 0 = leste, 90 = norte, 180 = oeste e 270 = sul
 
 tartaruga = Turtle()
 tela = Screen()
+tartaruga.pensize(10)
+tartaruga.speed(0) # Poderia colocar como string 0 = "fastest", 10 = "fast", 6 = "normal", 
+#"slow" = 3, "slowest" = 1    Exemplo: tartaruga.speed("fastest")
 
-tartaruga.shape("turtle")
-tartaruga.color("green")
-
-i = 3
-
-while i != 10:
-    tartaruga.pencolor(random.choice(cores.cores_variados))
-    for _ in range(i):
-        tartaruga.forward(100)
-        tartaruga.right(360/i) # Ângulo
-    i += 1
-
+for _ in range(300):
+    tartaruga.pencolor(r.choice(cores_variados))
+    tartaruga.forward(30)
+    tartaruga.setheading(r.choice(angulos))
+    
 tela.exitonclick()
