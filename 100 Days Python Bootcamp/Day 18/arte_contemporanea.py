520057a502ce128fbfbbf770c1672cf2578c55eb
Juntjh <thiagojunhonma@gmail.com>
2025-08-19 12:04:42 -0300

    hirst painting obtendo a lista de cores rgb - concluído

--- 100 Days Python Bootcamp/Day 18/arte_contemporanea.py
+++ 100 Days Python Bootcamp/Day 18/arte_contemporanea.py
@@ -1,5 +1,15 @@
 import colorgram
 
+rgb_cores = []
+
 cores = colorgram.extract("images.jpg", 15)
 
-print(cores)
+def cor_nova():
+    for cor_rgb in cores:
+        r = cor_rgb.rgb.r
+        g = cor_rgb.rgb.g
+        b = cor_rgb.rgb.b
+        nova_cor = (r, g, b)
+        rgb_cores.append(nova_cor)
+    return rgb_cores
+print(cor_nova())
