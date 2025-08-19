a0573c263c4cf112571681fe058848d025c9794f
Juntjh <thiagojunhonma@gmail.com>
2025-08-19 12:55:20 -0300

    hirst painting - conluído

--- 100 Days Python Bootcamp/Day 18/arte_contemporanea.py
+++ 100 Days Python Bootcamp/Day 18/arte_contemporanea.py
@@ -1,15 +1,25 @@
-import colorgram
+import turtle
+import random
 
-rgb_cores = []
+lista_cores_rgb = [(139, 80, 53), (185, 163, 125), (138, 166, 176), (60, 111, 134), (17, 42, 73), (139, 62, 88), (162, 153, 44), (66, 119, 101), (147, 182, 171), (215, 210, 107), (76, 34, 29)]
 
-cores = colorgram.extract("images.jpg", 15)
+tartaruga = turtle.Turtle()
+repeticao = 0
+posicao_x = -250
+posicao_y = -200
 
-def cor_nova():
-    for cor_rgb in cores:
-        r = cor_rgb.rgb.r
-        g = cor_rgb.rgb.g
-        b = cor_rgb.rgb.b
-        nova_cor = (r, g, b)
-        rgb_cores.append(nova_cor)
-    return rgb_cores
-print(cor_nova())
+turtle.colormode(255)
+tartaruga.penup()
+tartaruga.setposition(-250, -200)
+
+
+while repeticao != 10:
+    for _ in range(10):
+        tartaruga.dot(30, random.choice(lista_cores_rgb))
+        tartaruga.forward(50)
+        tartaruga.penup()
+    repeticao += 1
+    posicao_y += 50
+    tartaruga.setposition(posicao_x, posicao_y)
+    
+turtle.exitonclick()
