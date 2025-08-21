1d443ad4f08bb19d32f95f266369ead1a0b11fcb
Juntjh <thiagojunhonma@gmail.com>
2025-08-21 00:58:37 -0300

    desenhando - conluído

--- 100 Days Python Bootcamp/Day 19/day19.py
+++ 100 Days Python Bootcamp/Day 19/day19.py
@@ -6,6 +6,23 @@ tela = Screen()
 def mover_frente():
     tartaruga.forward(10)
 
+def mover_atrás():
+    tartaruga.backward(10)
+
+def sentido_horario():
+    tartaruga.right(10)
+
+def sentido_anti_horario():
+    tartaruga.left(10)
+
+def limpar_tela():
+    tartaruga.reset()
+
+tela.onkeypress(fun=mover_frente, key='w')
+tela.onkeypress(fun=mover_atrás, key='s')
+tela.onkeypress(fun=sentido_horario, key='d')
+tela.onkeypress(fun=sentido_anti_horario, key='a')
+tela.onkeypress(fun=limpar_tela, key='c')
 tela.listen()
-tela.onkey(key='space', fun=mover_frente)
+
 tela.exitonclick()
