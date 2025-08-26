be170b0191735a8779910e228d5d46a22982c6b9
Juntjh <thiagojunhonma@gmail.com>
2025-08-26 00:21:06 -0300

    da19 - corrida de tartaruga concluída

--- 100 Days Python Bootcamp/Day 19/day19.py
+++ 100 Days Python Bootcamp/Day 19/day19.py
@@ -2,9 +2,10 @@ from turtle import Turtle, Screen
 import random
 
 tela = Screen()
+corrida_continua = False
 
 tela.setup(width=700, height=500)
-aposta_usuario = tela.textinput(title="Corrida de Tartaruga", prompt="Faça a sua aposta, qual cor irá vencer?")
+aposta_usuario = tela.textinput(title="Corrida de Tartaruga", prompt="Faça a sua aposta, qual cor irá vencer?").lower()
 
 cores = ["red",
     "blue",
@@ -26,5 +27,21 @@ for cor_tartaruga in cores_aleatorios:
     tartaruga.goto(x=-300, y=pos_y)
     tartarugas.append(tartaruga)
     pos_y += 30
+
+if aposta_usuario:
+    corrida_continua = True
+
+while corrida_continua:
+    for tartaruga in tartarugas:
+        distancia_aleatoria = random.randint(0, 10)
+        tartaruga.forward(distancia_aleatoria)
+    if tartaruga.xcor() > 310:
+        cor_vencedor = tartaruga.pencolor().lower()
+        corrida_continua = False
+        if aposta_usuario == cor_vencedor:
+            print(f"Você acertou! A cor vencedora foi {cor_vencedor}!")
+        else:
+            print(f"Você errou! A cor vencedora foi {cor_vencedor}!")
+
 
 tela.exitonclick()
