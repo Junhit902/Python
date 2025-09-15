eb4b66063a652d37bd7e0cabdd115be6cd0b160a
Junhit902 <thiagojunhonma@gmail.com>
2025-09-15 00:02:52 -0300

    Usei o for loop para deixar o código mais legível, criando três objetos para serem posicionados para formar o corpo da cobrinha.

--- 100 Days Python Bootcamp/Day 20/jogo_cobrinha.py
+++ 100 Days Python Bootcamp/Day 20/jogo_cobrinha.py
@@ -1,18 +1,15 @@
 from turtle import Screen, Turtle
 
 tela = Screen()
-tartaruga = Turtle(shape="square")
-tartaruga2 = Turtle(shape="square")
-tartaruga3 = Turtle(shape="square")
 tela.setup(width=600, height=600)
 tela.bgcolor('Black')
 tela.title("Jogo da cobrinha")
 
-tartaruga.color("White")
-tartaruga.setpos(0, 0)
-tartaruga2.color("White")
-tartaruga2.setpos(-20, 0)
-tartaruga3.color("White")
-tartaruga3.setpos(-40, 0)
+posicoes_iniciais = [(0, 0), (-20, 0), (-40, 0)] # Uma lista de tuplos com as posições dos quadrados
+
+for posicao in posicoes_iniciais:
+    quadrado = Turtle(shape="square")
+    quadrado.color("white")
+    quadrado.setpos(posicao)
 
 tela.exitonclick()
